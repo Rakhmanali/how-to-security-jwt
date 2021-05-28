0437a21e4c6a6c67eb391fdea6061e2bb2481732
rakhmanali <rkabekov@gmail.com>
2021-05-28 13:43:42 +0500

    the sample of providing Spring Boot security by JWT

---
+++ src/main/java/com/example/demo/SecurityApplication.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// https://www.youtube.com/watch?v=her_7pa0vrg&t=15131s&ab_channel=Amigoscode
+@SpringBootApplication
+public class SecurityApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SecurityApplication.class, args);
+	}
+
+}
